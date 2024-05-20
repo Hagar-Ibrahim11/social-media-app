2ca5c1866715228c123092a2bbd528ad53d2a776
hagar <hagaribra04@gmail.com>
2024-05-20 18:29:46 +0300

    Add project files.

---
+++ social-media-app/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using social_media_app.Models;
+
+namespace social_media_app.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(User user)
+        {
+            await Clients.All
+                .SendAsync("ReceiveMessage", "User", $"{user.UserName} has joined");
+        }
+
+        //public async Task joinSpecificChat(User user)
+        //{
+        //    await Groups.AddToGroupAsync(Context.ConnectionId)
+        //}
+
+        public Task SendPrivateMessage(string userId, Message message)
+        {
+            return Clients.User(userId).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+    }
+}
